f27b3e30cc22f4734371d31f13d8583c18ffbd2a
Yaroslav <yarikmeisan@yandex.com>
2022-02-26 11:01:32 +0300

    Settign us Second Emotion (sadness)

--- HCJC-22/Assets/Scripts/Emotions.cs
+++ HCJC-22/Assets/Scripts/Emotions.cs
@@ -6,17 +6,28 @@ using UnityEngine;
 public class Emotions : MonoBehaviour
 {
     public GameObject scoreText;
-    public int joys;
     public AudioSource collectJoySound;
+    
+    [Header("Emotions")]
+    public int joys;
+    public int sadness;
+    
 
     private void OnTriggerEnter(Collider other)
     {
         print("touch " + other.tag);
+        
         if (other.CompareTag("Joy"))
         {
             //collectJoySound.Play();
             joys += 1; // Add Joys
             print("joys = " + joys);
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Sadness"))
+        {
+            sadness += 1; //Add Sadness
+            print("sadness = " + sadness);
             Destroy(other.gameObject);
         }
        
